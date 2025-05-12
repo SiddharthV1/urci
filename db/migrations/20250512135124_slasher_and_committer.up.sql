d5b06941a131e432ca5ff8f385561c380f0f56c1
Siddharth Vaderaa <svaderaa@gmail.com>
2025-05-12 16:48:52 +0200

    Slasher commitments

--- db/migrations/20250512135124_slasher_and_committer.up.sql
+++ db/migrations/20250512135124_slasher_and_committer.up.sql
@@ -1,9 +1,33 @@
-DROP TRIGGER IF EXISTS audit_log_trigger ON slasher;
-DROP TRIGGER IF EXISTS set_updated_at ON slasher;
 
-DROP TABLE IF EXISTS slasher;
+CREATE TABLE "slasher" (
+  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+  "address" CHAR(42),
+  "event_id" uuid,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now()),
+  "writer_id" uuid
+);
 
-DROP TRIGGER IF EXISTS audit_log_trigger ON committer;
-DROP TRIGGER IF EXISTS set_updated_at ON committer;
+ALTER TABLE "slasher" ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id");
 
-DROP TABLE IF EXISTS committer;
+ALTER TABLE "slasher" ADD FOREIGN KEY ("writer_id") REFERENCES "writers" ("id");
+
+select trigger_audit_log('slasher');
+select trigger_updated_at('slasher');
+
+CREATE TABLE "committer" (
+  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+  "address" CHAR(42),
+  "event_id" uuid,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now()),
+  "writer_id" uuid
+);
+
+
+ALTER TABLE "committer" ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id");
+
+ALTER TABLE "committer" ADD FOREIGN KEY ("writer_id") REFERENCES "writers" ("id");
+
+select trigger_audit_log('committer');
+select trigger_updated_at('committer');
