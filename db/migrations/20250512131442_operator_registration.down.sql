85f668144c1c00cfa4c6b854748f814b31bdfa6c
Siddharth Vaderaa <svaderaa@gmail.com>
2025-05-12 15:43:19 +0200

    Operator registration events

--- db/migrations/20250512131442_operator_registration.down.sql
+++ db/migrations/20250512131442_operator_registration.down.sql
@@ -4,5 +4,17 @@ DROP TRIGGER IF EXISTS set_updated_at ON operator_registration;
 
 DROP TABLE IF EXISTS operator_registrations;
 
+DROP TRIGGER IF EXISTS audit_log_trigger ON operator_registration_events;
+DROP TRIGGER IF EXISTS set_updated_at ON operator_registration_events;
+
+DROP TABLE IF EXISTS operator_registration_events;
 
 
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1 FROM pg_type WHERE typname = 'registration_status'
+  ) THEN
+    DROP TYPE registration_status;
+  END IF;
+END $$;
--- db/migrations/20250512131442_operator_registration.up.sql
+++ db/migrations/20250512131442_operator_registration.up.sql
@@ -1,3 +1,28 @@
+CREATE TYPE "registration_status" AS ENUM (
+  'register',
+  'unregister'
+);
+
+CREATE TABLE "operator_registration_events" (
+  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+  "event_id" uuid,
+  "operator_id" uuid,
+  "collateral" int,
+  "status" registration_status,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now()),
+  "writer_id" uuid
+);
+
+ALTER TABLE "operator_registration_events" ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id");
+
+ALTER TABLE "operator_registration_events" ADD FOREIGN KEY ("operator_id") REFERENCES "operators" ("id");
+
+ALTER TABLE "operator_registration_events" ADD FOREIGN KEY ("writer_id") REFERENCES "writers" ("id");
+
+SELECT trigger_audit_log('operator_registration_events');
+SELECT trigger_updated_at('operator_registration_events');
+
 CREATE TABLE "operator_registrations" (
   "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
   "event_id" uuid,
